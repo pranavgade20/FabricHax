b8d667078ed74345a1fc5cdfe4bf2abecde40e90
Pranav Gade <pranavgade20@gmail.com>
2020-08-01 03:35:27 +0530

    Made stuff much more cleaner, fixed mistakes

--- src/main/java/io/github/pranavgade20/autohotbar/BlockClickChange.java
+++ src/main/java/io/github/pranavgade20/autohotbar/BlockClickChange.java
@@ -2,6 +2,8 @@ package io.github.pranavgade20.autohotbar;
 
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
@@ -11,58 +13,65 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
-import net.minecraft.util.registry.Registry;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
 public class BlockClickChange implements AttackBlockCallback {
-    public HashMap<String, String> tools;
-    public BlockClickChange() {
-        tools = new HashMap<>();
-        tools.put("shovel", "[\\S\\s]*dirt [\\S\\s]*powder podzol gravel grass[\\S\\s]* farmland snow[\\S\\s]* [\\S\\s]*sand[\\S\\s]*");
-        tools.put("_axe", "[\\S\\s]*log [\\S\\s]*wood [\\S\\s]*planks [\\S\\s]*fence[\\S\\s]* [\\S\\s]*chest");
-        tools.put("pickaxe", "[\\S\\s]*stone[\\S\\s]* [\\S\\s]*diorite[\\S\\s]* [\\S\\s]*andesite[\\S\\s]* [\\S\\s]*ore[\\S\\s]* [\\S\\s]*nylium[\\S\\s]* [\\S\\s]*terracotta[\\S\\s]* [\\S\\s]*quartz[\\S\\s]* [\\S\\s]*ice[\\S\\s]* [\\S\\s]*prismarine[\\S\\s]*  [\\S\\s]*shulker_box hopper  [\\S\\s]*piston[\\S\\s]* [\\S\\s]*glass[\\S\\s]*");
-        tools.put("hoe", "[\\S\\s]*leaves[\\S\\s]* [\\S\\s]*wart[\\S\\s]*");
-        tools.put("shears", "[\\S\\s]*leaves[\\S\\s]* [\\S\\s]*wool[\\S\\s]* [\\S\\s]*carpet[\\S\\s]*");
-    }
     @Override
     public ActionResult interact(PlayerEntity playerEntity, World world, Hand hand, BlockPos blockPos, Direction direction) {
         PlayerInventory inventory = playerEntity.inventory;
+        BlockState block = world.getBlockState(blockPos);
         int slot = -1;
-        String blockName = Registry.BLOCK.getId(world.getBlockState(blockPos).getBlock()).getPath();
-        inv_loop : for (int i = 0; i < 9; i++) {
-            String itemName = Registry.ITEM.getId(inventory.getStack(i).getItem()).getPath();
-            for (Map.Entry<String, String> entry : tools.entrySet()) {
-                String tool = entry.getKey();
-                String blocks = entry.getValue();
-                for (String block : blocks.split(" ")) {
-                    if (Pattern.matches(block, blockName.toLowerCase())) {
-                        if (itemName.contains(tool)) {
-                            if (inventory.getStack(i).getDamage() + 1 != inventory.getStack(i).getMaxDamage()) {
-                                slot = i;
-                                break inv_loop;
-                            }
-                        }
+        float maxSpeed = Float.MIN_VALUE;
+        for (int i = 0; i < 9; i++) {
+            ItemStack tool = inventory.getStack(i);
+            if (isUsingEffectiveTool(tool, block)) {
+                float speed = getBlockBreakingSpeed(tool, block);
+                if (speed > maxSpeed) {
+                    if (inventory.getStack(i).getDamage() + 3 != inventory.getStack(i).getMaxDamage()) {
+                        maxSpeed = speed;
+                        slot = i;
                     }
                 }
             }
         }
 
-        if (slot != -1) {
-            int toScroll = 9 + (inventory.selectedSlot - slot);
-            toScroll %= 9;
-            for (int i = 0; i < toScroll; i++) {
-                inventory.scrollInHotbar(1);
+        if (slot == -1) {
+            slot = inventory.selectedSlot;
+            maxSpeed = getBlockBreakingSpeed(inventory.getStack(slot), block);
+            for (int i = 0; i < 9; i++) {
+                ItemStack tool = inventory.getStack(i);
+                float speed = getBlockBreakingSpeed(tool, block);
+                if (speed > maxSpeed) {
+                    if (inventory.getStack(i).getDamage() + 3 != inventory.getStack(i).getMaxDamage()) {
+                        maxSpeed = speed;
+                        slot = i;
+                    }
+                }
             }
+        }
+
+        int toScroll = 9 + (inventory.selectedSlot - slot);
+        toScroll %= 9;
+        for (int i = 0; i < toScroll; i++) {
+            inventory.scrollInHotbar(1);
         }
 
         return ActionResult.PASS;
     }
 
-    public boolean isUsingEffectiveTool(ItemStack item, BlockState block) { // modified from net.minecraft.entity.player.PlayerEntity.class
-        return !block.isToolRequired() || item.isEffectiveOn(block);
+    public boolean isUsingEffectiveTool(ItemStack tool, BlockState block) { // adapted from net.minecraft.entity.player.PlayerEntity.class
+        return !block.isToolRequired() || tool.isEffectiveOn(block);
     }
 
+    public float getBlockBreakingSpeed(ItemStack tool, BlockState block) { // adapted from net.minecraft.entity.player.PlayerEntity.class
+        float f = tool.getMiningSpeedMultiplier(block);
+
+        if (f > 1.0F) {
+            int efficiency = EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, tool);
+
+            if (efficiency > 0 && !tool.isEmpty()) {
+                f += (float) (efficiency * efficiency + 1);
+            }
+        }
+
+        return f;
+    }
 }
