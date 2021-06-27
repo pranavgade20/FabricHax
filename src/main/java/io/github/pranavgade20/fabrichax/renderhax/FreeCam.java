83b0cc7e7236c9e84dc487f3c295f341701785d6
Pranav Gade <pranavgade20@gmail.com>
2021-06-27 14:25:46 +0530

    fixed frecam

--- src/main/java/io/github/pranavgade20/fabrichax/renderhax/FreeCam.java
+++ src/main/java/io/github/pranavgade20/fabrichax/renderhax/FreeCam.java
@@ -3,6 +3,7 @@ package io.github.pranavgade20.fabrichax.renderhax;
 import io.github.pranavgade20.fabrichax.Settings;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
@@ -15,7 +16,7 @@ public class FreeCam extends RenderBase {
         INSTANCE = this;
     }
 
-    public static PlayerEntity e; // Do not rely on this being null
+    public static Entity e; // Do not rely on this being null
     @Override
     public boolean toggle() {
         if (enabled) {
@@ -23,11 +24,14 @@ public class FreeCam extends RenderBase {
             Settings.player.copyPositionAndRotation(e);
             Settings.world.removeEntity(e.getId(), Entity.RemovalReason.UNLOADED_WITH_PLAYER);
         } else {
-            e = new OtherClientPlayerEntity(Settings.world, Settings.player.getGameProfile());
-            e.setGlowing(true);
-            e.refreshPositionAndAngles(Settings.player.getX(), Settings.player.getY(), Settings.player.getZ(), Settings.player.getYaw(), Settings.player.getPitch());
-            e.resetPosition();
+            e = new ZombieEntity(Settings.world);
+            e.updatePositionAndAngles(Settings.player.getX(), Settings.player.getY(), Settings.player.getZ(), Settings.player.getYaw(), Settings.player.getPitch());
             Settings.world.addEntity(e.getId(), e);
+//            e = new OtherClientPlayerEntity(Settings.world, Settings.player.getGameProfile());
+//            e.setGlowing(true);
+//            e.updatePositionAndAngles(Settings.player.getX(), Settings.player.getY(), Settings.player.getZ(), Settings.player.getYaw(), Settings.player.getPitch());
+//            e.resetPosition();
+//            Settings.world.addEntity(e.getId(), e);
 
             Settings.player.setBoundingBox(new Box(Settings.player.getPos(), Settings.player.getPos()));
             flying = Settings.player.getAbilities().flying;
